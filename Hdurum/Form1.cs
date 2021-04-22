dc13deeefd400baeabd62da8505975d8177a239b
MuhammetAliKURUTAY <m.ali-kurutay@hotmail.com>
2021-04-22 21:37:26 +0300

    xml çekildi.

--- Hdurum/Form1.cs
+++ Hdurum/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 using System.Windows.Forms;
 
 namespace Hdurum
@@ -15,6 +17,15 @@ namespace Hdurum
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlTextReader reader = new XmlTextReader("https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml");
+            DataSet data = new DataSet();
+            data.ReadXml(reader);
+            dataGridView1.DataSource = data.Tables[1];
+
         }
     }
 }
