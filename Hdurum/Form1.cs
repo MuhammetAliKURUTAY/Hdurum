b078be5f25fdad8e9ddd8c4a967d958f9e42ec5b
MuhammetAliKURUTAY <m.ali-kurutay@hotmail.com>
2021-04-22 22:14:14 +0300

    çelilen xml metin belgesine yazdırıldı.

--- Hdurum/Form1.cs
+++ Hdurum/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Hdurum
@@ -21,11 +22,25 @@ namespace Hdurum
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             XmlTextReader reader = new XmlTextReader("https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml");
             DataSet data = new DataSet();
             data.ReadXml(reader);
             dataGridView1.DataSource = data.Tables[1];
+            TextWriter writer = new StreamWriter(@"Text.txt");
+
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    writer.Write("\t" + dataGridView1.Rows[i].Cells[j].Value.ToString() + "\t" + "|");
+                }
+                writer.WriteLine("");
+                writer.WriteLine("-----------------------------------------------------");
+            }
+            writer.Close();
 
         }
     }
 }
+
